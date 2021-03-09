3b74b8dd8b0491b74b836f8da8273099325e6787
Nikhil Jindia <nikhiljindia@gmail.com>
2021-03-09 16:21:25 -0500

    getAvgHist finally works poggers

--- fabric_detection.py
+++ fabric_detection.py
@@ -55,33 +55,37 @@ def getImgs(parent_folder_path):
 
     return imgs
 
+def getHOG(img):
+    hist = hog(img, orientations=36, pixels_per_cell=(50, 50), cells_per_block=(1, 1))
+    return hist
+
+def roi(img):
+    return img[1000:1050, 1200:1250]
+
 def getAvgHist(images):
     histArr = []
+    csvArr = []
     for image in images:
-        img = increaseContrast(image.image)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        kernel_size = 19
-        gaussian = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)  # GOOD! Reduces noise
-      
-        low_threshold = 50
-        high_threshold = 150
-        edges = cv2.Canny(gaussian, low_threshold, high_threshold)
-        hist, hog_image = hog(edges, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), visualize=True)
+        img = roi(image.image)
+        l = getLChannel(img)
+        cv2.imwrite('images/smol/' + image.fileName, l)
+        hist = getHOG(l)
+        np.savetxt(image.fileName + '.csv', hist, fmt='%s', delimiter=',', header="value")
         histArr.append(hist)
-        print(hist)
-    sum = [0]
+        csvArr = np.append(csvArr, np.full(len(hist), image.fileName), axis=0)
+        print(len(hist))
+    flatArr = [elem for hist in histArr for elem in hist]
+    print(flatArr)
+    arr = (csvArr, flatArr)
+    arr = np.transpose(arr)
+    np.savetxt('imgs.csv', arr, fmt='%s', delimiter=',', header="hist, value")
+    sum = np.full(len(histArr[0]), 0)
     avg = []
     for hist in histArr:
-        pass
+        sum = sum + hist
+    avg = sum / len(histArr)
     return avg
 
-def mean(hist):
-    mean = 0.0
-    for i in hist:
-        mean += i
-    mean = mean / len(hist)
-    return mean
 
 def getIntersection(hist_1, hist_2):
         """
@@ -98,51 +102,35 @@ def getIntersection(hist_1, hist_2):
 def getKStest(hist1, hist2):
     kStat, p = ks_2samp(hist1, hist2)
     return kStat, p
-
-def bhattacharyya(hist1, hist2, bins):
-    bhatt_coeff = 0
-    for h1, h2 in zip(hist1, hist2):
-        sqrt_of_product = h1*h2
-        bhatt_coeff = bhatt_coeff + sqrt_of_product
-    print("bhat_coeff: " + str(bhatt_coeff))
-    hellinger = np.sqrt(1-bhatt_coeff)
-    print("hellinger: " + str(hellinger))
-    # hist1_mean = mean(hist1)
-    # hist2_mean = mean(hist2)
-    # p2 = 1/(np.sqrt(hist1_mean * hist2_mean * bins))
-    # p3 = 0.0
-    # for h1, h2 in zip(hist1, hist2):
-    #     p3 = p3 + np.sqrt(h1 * h2)
-    # p = 1 - (p2 * p3)
-    # print(p)
-    bhatta = -1 * np.log(bhatt_coeff)
-    print("bhatta_dist: " + str(bhatta))
         
 def getLChannel(img):
     lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(lab)
     return l
 
-def compareHist(img1, img2):
-    # fileName = image1.fileName + '&&' + image2.fileName + '.csv'
+def compareHist(img1, avgHist):
+    fileName = img1.fileName + '.csv'
     img1 = img1.image
     # img2 = img2.image
     np.set_printoptions(threshold=sys.maxsize)
-    img1 = img1[1000:1100, 1200:1300]
-    img2 = img2[1000:1100, 1200:1300]
+    img1 = roi(img1)
+    img2 = roi(img2)
     l1= getLChannel(img1)
     l2= getLChannel(img2)
 
     cv2.imwrite('images/smol/present.png', img1)
     cv2.imwrite('images/smol/not_present.png', img2)
-    bins = 1000
-    hist1 = hog(l1, orientations=bins, pixels_per_cell=(25, 25), cells_per_block=(1, 1))
-    hist2 = hog(l2, orientations=bins, pixels_per_cell=(25, 25), cells_per_block=(1, 1))    
-    # hists = np.append(hist1, hist2)
-    # catArr = np.append(np.full(len(hist1), 'Present L'), np.full(len(hist2), 'Not Present L'))
-    # arr = (catArr, hists)
-    # arr = np.transpose(arr)
-    # np.savetxt(fileName, arr, fmt='%s', delimiter=',', header="hist, value")
+    hist1, imgH = getHOG(l1)
+    hist2, imgH2 = getHOG(l2)
+    cv2.imshow('imgH', imgH)
+    cv2.imshow('imgH2', imgH2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+    hists = np.append(hist1, hist2)
+    catArr = np.append(np.full(len(hist1), 'Present L'), np.full(len(hist2), 'Not Present L'))
+    arr = (catArr, hists)
+    arr = np.transpose(arr)
+    np.savetxt(fileName, arr, fmt='%s', delimiter=',', header="hist, value")
 
     # bhattacharyya(hist1, hist2, bins)
     # intersection = getIntersection(hist1, hist2)
@@ -151,23 +139,27 @@ def compareHist(img1, img2):
     return kStat, p
 
 
-
 def main():
     i = 0
     # imgs = getImgs('images/dark_tests/dblue/outside')
-    present = getImgs('images/darks')
-    # avgPresentHist = getAvgHist(present)
-    # refPic = cv2.imread('images/fabric_not_present/destacker_back_20201112_163305_WO0176_7377-BP_Inside.png', 1)
-    refPic = cv2.imread('images/darks/db_10000.png', 1)
-    # darkPic = cv2.imread('images/dark_contrast.png', 1)
+    # present = getImgs('images/darks')
     notPresent = getImgs('images/fabric_not_present')
-    # avgNotPresentHist = getAvgHist(notPresent)
-    print("present")
-    for img in present:
-        print(compareHist(img, refPic))
-    print("not present")
-    for img in notPresent:
-        print(compareHist(img, refPic))
-    # print(compareHist(darkPic, refPic))
+
+    avgPresentHist = getAvgHist(notPresent)
+    np.savetxt('avgHist.csv', avgPresentHist, fmt='%s', delimiter=',', header="value")
+    print('checkpoint')
+    
+    # # refPic = cv2.imread('images/fabric_not_present/destacker_back_20201112_163305_WO0176_7377-BP_Inside.png', 1)
+    # refPic = cv2.imread('images/darks/db_10000.png', 1)
+    # # darkPic = cv2.imread('images/dark_contrast.png', 1)
+    # notPresent = getImgs('images/fabric_not_present')
+    # # avgNotPresentHist = getAvgHist(notPresent)
+    
+    # print("present")
+    # for img in present:
+    #     print(compareHist(img, refPic))
+    # print("not present")
+    # for img in notPresent:
+    #     print(compareHist(img, refPic))
 if __name__ == '__main__':
     main()
